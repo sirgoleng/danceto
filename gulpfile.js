db4ca7bf58a4ba3ac3383b0dce4c2f7c9a0694b1
sirgoleng <sirgoleng@gmail.com>
2017-11-09 09:27:59 +0800

    office

--- gulpfile.js
+++ gulpfile.js
@@ -7,8 +7,8 @@ var uglify = require('gulp-uglify');//js代码压缩
 var csso = require('gulp-csso');//css代码压缩
 
 gulp.task('default', function(){
-    var jsFilter = filter('**/*.js',{restore: true});
-    var cssFilter = filter('**/*.css',{restore: true});
+    var jsFilter = filter('js/*.js',{restore: true});
+    var cssFilter = filter('css/*.css',{restore: true});
     var indexHtmlFilter = filter(['**/*', '!**/index.htm'],{restore: true});
 
     return gulp.src('index.htm')
