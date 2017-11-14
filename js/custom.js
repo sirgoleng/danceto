bdf7c40377702556e96e128d1e6af169d6adb909
sirgoleng <sirgoleng@gmail.com>
2017-11-14 15:35:12 +0800

    office

--- js/custom.js
+++ js/custom.js
@@ -1,13 +1,13 @@
 $(function () {
-    var nav = $(".header-title"); //得到导航对象
+    var nav = $(".jumbotron"); //得到导航对象
     var win = $(window); //得到窗口对象
     var sc = $(document);//得到document文档对象。
     win.scroll(function () {
-        if (sc.scrollTop() >= 52) {
-            nav.addClass("fixednav");
+        if (sc.scrollTop() >= 100) {
+            nav.addClass("blur");
             //$(".nav").fadeIn();
         } else {
-            nav.removeClass("fixednav");
+            nav.removeClass("blur");
             //$(".nav").fadeOut();
         }
     })
